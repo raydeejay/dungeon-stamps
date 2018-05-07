43f7aa1f717541f72e575ca90ea4a6de10911a7e
Sergi Reyner <sergi.reyner@gmail.com>
2018-05-07 15:21:36 +0100

    Added scene manager, code reorganisation
    Also added chests which generate coins when opened

--- dungeon-stamps.lisp
+++ dungeon-stamps.lisp
@@ -4,6 +4,16 @@
 
 ;;; "dungeon-stamps" goes here. Hacks and glory await!
 
+(defparameter *canvas-width* 400)
+(defparameter *canvas-height* 600)
+
+(gamekit:defgame dungeon-stamps () ()
+                 (:viewport-width *canvas-width*)     ; window's width
+                 (:viewport-height *canvas-height*)   ; window's height
+                 (:viewport-title "Dungeon stamps"))  ; window's title
+
+(gamekit:register-resource-package 'dungeon-stamps "/home/raydj/lisp/dungeon-stamps/img")
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; GAME STATE
 (defparameter *hero* nil)
@@ -15,32 +25,37 @@
   ((name :accessor name :initarg :name :initform 'stamp))
   (:documentation "Base clase for stamps."))
 
+(defclass weapon (stamp)
+  ((name   :initform 'weapon)
+   (damage :accessor damage :initarg  :damage :initform (+ 3 (random 8)))))
+
+(defclass demon (stamp)
+  ((name :accessor name :initform 'demon)
+   (hp   :accessor hp   :initarg  :hp    :initform (1+ (random 6)))))
+
+(defclass coin (stamp)
+  ((name  :initform 'coin)
+   (value :accessor value :initarg  :value :initform (1+ (random 10)))))
+
+(defclass potion (stamp)
+  ((name :accessor name :initform 'potion)))
+
+(defclass chest (stamp)
+  ((name  :accessor name  :initform 'chest)))
+
+(defclass hero (stamp)
+  ((name   :accessor name   :initform 'hero)
+   (coins  :accessor coins  :initarg  :coins  :initform 0)
+   (hp     :accessor hp     :initarg  :hp     :initform *maxhp*)
+   (weapon :accessor weapon :initarg  :weapon :initform nil)))
+
+
 (defgeneric interact (stamp)
   (:documentation "Interact with a stamp. Returns T if the stamp should be discarded."))
-
-(defgeneric draw-stamp (stamp x y)
-  (:documentation "Draws the stamp at the specified position."))
-
-(defmethod draw-stamp ((stamp stamp) x y)
-  (draw-image (vec2 (* 120 x) (* y 160))
-              (name stamp)))
-
-
-(defclass weapon (stamp)
-  ((name   :accessor name   :initform :weapon)
-   (damage :accessor damage :initarg  :damage :initform (+ 3 (random 8)))))
 
 (defmethod interact ((stamp weapon))
   (setf (weapon *hero*) stamp)
   T)
-
-(defmethod draw-stamp :after ((stamp weapon) x y)
-  (print-text (format nil "~D" (damage stamp)) (+ 10 (* 120 x)) (+ 10 (* y 160))))
-
-
-(defclass demon (stamp)
-  ((name :accessor name :initform :demon)
-   (hp   :accessor hp   :initarg  :hp    :initform (1+ (random 10)))))
 
 (defmethod interact ((stamp demon))
   (if (weapon *hero*)
@@ -57,55 +72,47 @@
       (progn (decf (hp *hero*) (hp stamp))
              T)))
 
-(defmethod draw-stamp :after ((stamp demon) x y)
-  (print-text (format nil "~D" (hp stamp)) (+ 90 (* 120 x)) (+ 140 (* y 160))))
-
-
-(defclass coin (stamp)
-  ((name  :accessor name  :initform :coin)
-   (value :accessor value :initarg  :value :initform (1+ (random 10)))))
-
 (defmethod interact ((stamp coin))
-  (incf *coins* (value stamp))
+  (incf (coins *hero*) (value stamp))
   T)
 
-(defmethod draw-stamp :after ((stamp coin) x y)
-  (print-text (format nil "~D" (value stamp)) (+ 90 (* 120 x)) (+ 10 (* y 160))))
-
-
-(defclass potion (stamp)
-  ((name :accessor name :initform :potion)))
+(defmethod interact ((stamp chest))
+  (change-class stamp 'coin :name 'coin :value (1+ (random 10)))
+  nil)
 
 (defmethod interact ((stamp potion))
   (setf (hp *hero*) *maxhp*)
   T)
 
+(defgeneric draw-stamp (stamp x y)
+  (:documentation "Renders the stamp at the specified position. Specialise :after methods for
+additional rendering, like health points or damage value."))
 
-(defclass hero (stamp)
-  ((name   :accessor name   :initform :hero)
-   (coins  :accessor coins  :initarg  :coins  :initform 0)
-   (hp     :accessor hp     :initarg  :hp     :initform *maxhp*)
-   (weapon :accessor weapon :initarg  :weapon :initform nil)))
+(defmethod draw-stamp ((stamp stamp) x y)
+  (draw-image (vec2 (* 120 x) (* y 160))
+              (name stamp)))
+
+(defmethod draw-stamp :after ((stamp weapon) x y)
+  (print-text (format nil "~D" (damage stamp)) (+ 10 (* 120 x)) (+ 10 (* y 160))))
+
+(defmethod draw-stamp :after ((stamp demon) x y)
+  (print-text (format nil "~D" (hp stamp)) (+ 90 (* 120 x)) (+ 140 (* y 160))))
+
+(defmethod draw-stamp :after ((stamp coin) x y)
+  (print-text (format nil "~D" (value stamp)) (+ 90 (* 120 x)) (+ 10 (* y 160))))
 
 (defmethod draw-stamp :after ((stamp hero) x y)
   (print-text (format nil "~D" (hp stamp)) (+ 90 (* 120 x)) (+ 140 (* y 160)))
   (when (weapon stamp)
     (print-text (format nil "~D" (damage (weapon stamp))) (+ 10 (* 120 x)) (+ 10 (* y 160)))))
 
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; UI
-(defparameter *canvas-width* 400)
-(defparameter *canvas-height* 600)
-
 (defparameter *cursor-position* (gamekit:vec2 0 0))
 
 (defparameter *current-slot* 4)
 (defparameter *clicked-slot* -1)
-
-(gamekit:defgame dungeon-stamps () ()
-  (:viewport-width *canvas-width*)     ; window's width
-  (:viewport-height *canvas-height*)   ; window's height
-  (:viewport-title "Dungeon stamps"))  ; window's title
 
 (defun coords-to-slot (coords)
   (let ((x (x coords))
@@ -147,7 +154,76 @@
 (defun try-to-move ()
   (when (can-move *current-slot* *clicked-slot*)
     (when (interact (aref *field* *clicked-slot*))
-      (move-to-target))))
+      (move-to-target)))
+  (when (< (hp *hero*) 1)
+    (switch-to-scene (make-instance 'menu))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; SCENES
+(defparameter *current-scene* nil)
+
+(defclass scene () ())
+(defgeneric on-enter (scene))
+(defgeneric on-act (scene))
+(defgeneric on-draw (scene))
+(defgeneric on-exit (scene))
+
+(defmethod on-enter ((scene scene))
+  (gamekit:bind-cursor (lambda (x y)
+                         "Keep track of the pointer position."
+                         (setf *cursor-position* (vec2 x y)))))
+
+
+;; menu
+(defclass menu (scene) ())
+(defmethod on-enter :after ((scene menu))
+  (gamekit:bind-button :mouse-left :pressed
+                       (lambda ()
+                         "Activate the clicked widget, if there's one."
+                         (when (eql 4 (coords-to-slot *cursor-position*))
+                           (switch-to-scene (make-instance 'game))))))
+
+(defmethod on-act ((scene menu)))
+(defmethod on-draw ((scene menu))
+  (print-text "START" 150 200))
+
+(defmethod on-exit ((scene menu)))
+
+
+;; game
+(defclass game (scene) ())
+(defmethod on-enter :after ((scene game))
+  (gamekit:bind-button :mouse-left :pressed
+                       (lambda ()
+                         "Try to move to another slot"
+                         ;;(gamekit:play :snake-grab)
+                         (setf *clicked-slot* (coords-to-slot *cursor-position*))
+                         (try-to-move)))
+  (init-game))
+
+(defmethod on-act ((scene game)))
+
+(defmethod on-draw ((scene game))
+  (gamekit:print-text (format nil "Coins: ~D / HP: ~D" (coins *hero*) (hp *hero*)) 0 500)
+  (loop :for y :from 0 :upto 2 :do
+     (loop :for x :from 0 :upto 2 :do
+        (draw-stamp (aref *field-grid* y x) x y))))
+
+(defmethod on-exit ((scene game)))
+
+
+;; death scene
+(defclass death (scene) ())
+(defmethod on-enter ((scene death)))
+(defmethod on-act ((scene death)))
+(defmethod on-draw ((scene death)))
+(defmethod on-exit ((scene death)))
+
+(defun switch-to-scene (scene)
+  (when *current-scene*
+    (on-exit *current-scene*))
+  (setf *current-scene* scene)
+  (on-enter *current-scene*))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; INITIALISATION
@@ -155,10 +231,11 @@
 (defparameter *field-grid* nil)
 
 (defun generate-stamp ()
-  (make-instance (random-elt '(coin coin coin coin coin
-                               demon demon demon demon demon demon demon demon demon demon
+  (make-instance (random-elt '(coin coin coin coin
+                               demon demon demon demon demon demon demon
                                potion
-                               weapon))))
+                               chest
+                               weapon weapon weapon))))
 
 (defun make-field ()
   (let ((field (make-array '(9))))
@@ -168,58 +245,36 @@
     field))
 
 (defun load-resources ()
-  (gamekit:register-resource-package :keyword "img/")
   ;; (gamekit:define-sound :snake-grab "snake-grab.ogg")
-  (gamekit:define-image :hero "hero.png")
-  (gamekit:define-image :demon "demon.png")
-  (gamekit:define-image :coin "coin.png")
-  (gamekit:define-image :potion "potion.png")
-  (gamekit:define-image :weapon "weapon.png")
-  (gamekit:define-image :empty "empty.png")
-  )
-
-(defun bind-controls ()
-  (gamekit:bind-cursor (lambda (x y)
-                         "When left mouse button is pressed, update snake's head position"
-                         (setf *cursor-position* (vec2 x y))))
-  (gamekit:bind-button :mouse-left :pressed
-                       (lambda ()
-                         "Try to move to another slot"
-                         ;;(gamekit:play :snake-grab)
-                         (setf *clicked-slot* (coords-to-slot *cursor-position*))
-                         (try-to-move)))
-  ;;(gamekit:bind-button :mouse-left :released (lambda () ))
+  (gamekit:define-image 'hero "hero.png")
+  (gamekit:define-image 'demon "demon.png")
+  (gamekit:define-image 'coin "coin.png")
+  (gamekit:define-image 'potion "potion.png")
+  (gamekit:define-image 'weapon "weapon.png")
+  (gamekit:define-image 'chest "chest.png")
+  (gamekit:define-image 'empty "empty.png")
   )
 
 (defun init-game ()
   (setf *field* (make-field)
         *field-grid* (make-array '(3 3) :displaced-to *field*)
-        *coins* 0
-        *kills* 0
         *hero* (make-instance 'hero)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; HOOKS
 (defmethod gamekit:act ((app dungeon-stamps))
-  ;; possibly do something here
+  ;; (when *current-scene* (on-act *current-scene*))
   )
 
-
 (defmethod gamekit:draw ((app dungeon-stamps))
-  (gamekit:print-text (format nil "Coins: ~D / Kills: ~D / HP: ~D" *coins* *kills* (hp *hero*)) 0 500)
-  ;; let's center image position properly first
-  ;; (let ((head-image-position (gamekit:subt (aref *curve* 3) (gamekit:vec2 32 32))))
-  ;;   ;; then draw it where it belongs
-  ;;   (gamekit:draw-image head-image-position :snake-head))
-
-  (loop :for y :from 0 :upto 2 :do
-     (loop :for x :from 0 :upto 2 :do
-        (draw-stamp (aref *field-grid* y x) x y))))
+  (when *current-scene* (on-draw *current-scene*)))
 
 (defmethod gamekit:post-initialize ((app dungeon-stamps))
   (load-resources)
-  (bind-controls)
-  (init-game))
+  (switch-to-scene (make-instance 'menu))
+  ;; (bind-controls)
+  ;; (init-game)
+  )
 
 (defun launch ()
   (gamekit:start 'dungeon-stamps))
